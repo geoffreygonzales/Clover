e760351eef5f78e618a165e7994b20c301842f80
Geoffrey Gonzales <geoffreygonzales@Geoffreys-MacBook-Pro.local>
2017-10-01 14:49:46 -0700

    base app done. maybe add onboarding, and artwork

--- Clover/View/ViewController.swift
+++ Clover/View/ViewController.swift
@@ -7,28 +7,94 @@
 //
 
 import UIKit
+import AudioToolbox
+import CoreMotion
 
 class ViewController : UIViewController
 {
           @IBOutlet weak var countLabel: UILabel!
           @IBOutlet weak var clearButton: UIButton!
           @IBOutlet weak var cancelClear: UIButton!
+          @IBOutlet var swipeLeft: UISwipeGestureRecognizer!
+          @IBOutlet weak var normalCountButton: UIButton!
+          
+          // Universal Objects
           var count = 0
+          var hapticAvaliable = false
           
           // 3D Touch Objects
           var impactFinished = false
           let generatorHeavy = UIImpactFeedbackGenerator(style: .heavy)
           let generatorLight = UIImpactFeedbackGenerator(style : .light)
-          let pressureThreshold : CGFloat = 0.3
+          let pressureThreshold : CGFloat = 0.65
           
           override func viewDidLoad()
           {
                     super.viewDidLoad()
+                    
+                    if  #available(iOS 9.0, *),  traitCollection.forceTouchCapability == UIForceTouchCapability.available
+                    {
+                              normalCountButton.isHidden = true
+                              hapticAvaliable = true
+                              
+                              print("haptic is avaliable")
+                    }
+                    
+                    updateLabel(text: "hi")
           }
           
+          // Passing In Value To Lable And Displaying It
           func updateLabel(text : Any)
           {
                     countLabel.text = "\(text)"
+          }
+          
+          // Adding 1 To The Count And Displaying It
+          func countOne()
+          {
+                    count = count + 1
+                    updateLabel(text: count)
+          }
+          
+          func minusOne()
+          {
+                    count = count - 1
+                    updateLabel(text: count)
+          }
+          
+          //  Single Vibration
+          func buzz()
+          {
+                    if hapticAvaliable == false
+                    {
+                              AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                              print("buzz")
+                    }
+          }
+          
+          // Double Vibration
+          func buzzBuzz()
+          {
+                    if hapticAvaliable == false
+                    {
+                              if normalCountButton.isHidden == false
+                              {
+                                        normalCountButton.isHidden = true
+                                        print("normal count button is hidden")
+                              }
+                              else
+                              {
+                                        normalCountButton.isHidden = false
+                                        print("normal count button is shown")
+                              }
+                              for _ in 1...2
+                              {
+                                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                                        usleep(350_000)
+                              }
+                              
+                              print("buzz buzz ladies")
+                    }
           }
           
           // 3d Touch For iPhone 6 And Newer
@@ -37,23 +103,29 @@ class ViewController : UIViewController
           {
                     generatorLight.prepare()
                     generatorHeavy.prepare()
+                    
                     print("preparing to impact")
           }
           
-          // Heavy Impact Feeback Snap
+          // Heavy Impact Feeback
           func impactHeavy()
           {
-                    generatorHeavy.impactOccurred()
-                    generatorHeavy.impactOccurred()
                     generatorHeavy.impactOccurred()
                     print("heavy impact")
           }
           
-          // Light Impact Feeback Snap
+          // Light Impact Feeback
           func impactLight()
           {
                     generatorLight.impactOccurred()
                     print("light impact")
+          }
+          
+          // Alert Impact Feeback
+          func impactClear()
+          {
+                    let generator = UINotificationFeedbackGenerator()
+                    generator.notificationOccurred(.error)
           }
           
           override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -74,9 +146,12 @@ class ViewController : UIViewController
                                                   
                                                   if force > pressureThreshold
                                                   {
-                                                            self.impactFinished = true
+                                                            countOne()
+                                                            
                                                             self.impactHeavy()
                                                             self.prepareImpact()
+                                                            
+                                                            self.impactFinished = true
                                                   }
                                         
                                         // Lifting Up Triggering Light Snap Feedback
@@ -84,45 +159,81 @@ class ViewController : UIViewController
                                                   
                                                   if force < pressureThreshold
                                                   {
-                                                            self.impactFinished = false
                                                             self.impactLight()
                                                             self.prepareImpact()
+                                                            
+                                                            self.impactFinished = false
                                                   }
                               }
                     }
           }
           
-//          @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer)
-//          {
-//                    updateLabel(text: "clear")
-//                    countLabel.isHidden = true
-//                    clearButton.isHidden = false
-//                    cancelClear.isHidden = false
-//                    print("clear")
-//          }
-
+          @IBAction func normalCountButtonTapped(_ sender: UIButton)
+          {
+                    countOne()
+                    buzz()
+          }
+          
           @IBAction func swipeLeft(_ sender: Any)
           {
-                    countLabel.isHidden = true
-                    clearButton.isHidden = false
-                    cancelClear.isHidden = false
-                    print("clear")
+                    if count != 0, hapticAvaliable == true
+                    {
+                              countLabel.isHidden = true
+                              clearButton.isHidden = false
+                              cancelClear.isHidden = false
+                              swipeLeft.isEnabled = false
+                              
+                              impactClear()
+                              
+                              print("swipe left")
+                    }
+          }
+          
+          // Phone Is Shaken
+          override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?)
+          {
+                    if count != 0
+                    {
+                              normalCountButton.isHidden = true
+                              countLabel.isHidden = true
+                              clearButton.isHidden = false
+                              cancelClear.isHidden = false
+                              swipeLeft.isEnabled = false
+                              
+                              impactClear()
+                              
+                              print("phone shook like fi + se")
+                    }
           }
           
           @IBAction func clearButton(_ sender: UIButton)
           {
-                    updateLabel(text: 0)
+                    count = 0
+                    updateLabel(text: count)
                     countLabel.isHidden = false
                     clearButton.isHidden = true
                     cancelClear.isHidden = true
+                    swipeLeft.isEnabled = true
+                    
+                    impactClear()
+                    buzzBuzz()
+          
                     print("clear pressed")
           }
           
           @IBAction func cancelClear(_ sender: UIButton)
           {
+                    if hapticAvaliable == false
+                    {
+                              normalCountButton.isHidden = false
+                    }
                     countLabel.isHidden = false
                     clearButton.isHidden = true
                     cancelClear.isHidden = true
+                    swipeLeft.isEnabled = true
+                    
+                    impactLight()
+
                     print("cancel clear")
           }
 }
